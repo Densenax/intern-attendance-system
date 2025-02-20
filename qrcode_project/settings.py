e2e148d093480827d91e7e866393f0e860bef48f
IcySkype <skytrizero@gmail.com>
2025-02-20 13:55:09 +0800

    changed database to postgresql-superbase

--- qrcode_project/settings.py
+++ qrcode_project/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = "qrcode_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require', 
+        },
     }
 }
 
